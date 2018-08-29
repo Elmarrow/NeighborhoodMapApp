bdcfda996f11e26e9b0714b576724e2c063cfe4c
Aggelos Afratis <aafratis@gmail.com>
2018-08-29 03:39:48 +0300

    Completed filtering logic

---
+++ src/Filtering.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+
+function Filtering(props){
+    let locationsList = props.FilteredLocations.map((location) =>
+	<li className="myLi" tabIndex="0" key={location.venueID} onClick={()=> props.handleLocationClick(location.venueID)}><a>{location.title}</a></li>
+	);
+
+return (
+      <div>
+      <form>
+        <label>
+          <input type="text" id="searchField" placeholder="Search" onChange={(event) => props.handleTextFilter(event)} />
+          <ul id="venuesList">
+            {locationsList}
+          </ul>
+        </label>
+      </form>
+      </div>
+  );
+
+}
+
+export default Filtering;
